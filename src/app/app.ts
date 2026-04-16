5e3f7b7ccf415523f2403e2c09f18e0b6db12e96
Nichola Bauwelinck <nichola.bauwelinck@student.kdg.be>
2026-04-16 17:32:35 +0200

    feat: add initial design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZardCardComponent } from "./shared/components/card";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ZardCardComponent } from "@/shared/components/card";
+
+@Component({
+  selector: 'app-home',
+  imports: [ZardCardComponent],
+  templateUrl: './home.html'
+})
+export class Home { }
